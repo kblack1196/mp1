c6812ec519d43e72ae6c17038f48f4441cad5e55
unknown <kblack1196@naver.com>
2025-04-15 14:05:34 +0900

    test

---
+++ MidtermTest/MidtermTest/20221401/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	printf("                                                                           ");
+	return 0;
+}
